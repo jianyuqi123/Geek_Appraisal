262c5831a1e3b912e3e3e25eb6033c98b49c9c5a
Jianyuqi123 <2625777857@qq.com>
2022-11-13 18:47:21 +0800

    Geek_Appraisal

--- task1/main.c
+++ task1/main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include<math.h>
+int main()
+{
+    double temp,buChang = 0.0001;
+    double yStart = 1,y = 1;
+    // scanf("%lf %lf",&yStart,&buChang);
+    temp = buChang;
+    printf("x          y\n");
+    while(y > 0.0001||y < -0.0001)
+    {
+        y = yStart*exp(-buChang);
+        printf("%lf     %lf\n",buChang,y);
+        buChang += temp;
+    }
+return 0;
 
-int main(int argc, char *argv[]) {
-	printf("Hello World!\n");
-	return 0;
 }
-
